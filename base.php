c0e737616f048636e59b126740acd95dd698e3e6
Damien Lesgourgues <damien.lesgourgues@believedigital.com>
2017-11-30 18:16:43 +0100

    Start of error management

--- base.php
+++ base.php
@@ -2,44 +2,58 @@
 
 function doGetRequest($uri, $token = '') {
   $curl = curl_init($uri);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($curl, CURLOPT_HEADER, false);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_HEADER, FALSE);
   curl_setopt($curl, CURLOPT_TIMEOUT, 4);
   if ($token !== '') { curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: OAuth $token")); };
-  $return = curl_exec($curl);
+  $ret = curl_exec($curl);
   curl_close($curl);
-  return json_decode($return, true);
+  $return = json_decode($ret, TRUE);
+  if (isset($return['error'])) {
+    return FALSE;
+  } else {
+    return $return;
+  }
 }
 
 function doPostRequest($uri, $token = '', array $data = array()) {
   $curl = curl_init($uri);
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($curl, CURLOPT_FORBID_REUSE, true);
-  curl_setopt($curl, CURLOPT_FRESH_CONNECT, true);
-  curl_setopt($curl, CURLOPT_HEADER, false);
+  curl_setopt($curl, CURLOPT_POST, TRUE);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_FORBID_REUSE, TRUE);
+  curl_setopt($curl, CURLOPT_FRESH_CONNECT, TRUE);
+  curl_setopt($curl, CURLOPT_HEADER, FALSE);
   curl_setopt($curl, CURLOPT_TIMEOUT, 4);
   if ($token !== '') { curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: OAuth $token")); };
   curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-  $return = curl_exec($curl);
+  $ret = curl_exec($curl);
   curl_close($curl);
-  return json_decode($return, true);
+  $return = json_decode($ret, TRUE);
+  if (isset($return['error'])) {
+    return FALSE;
+  } else {
+    return $return;
+  }
 }
 
 function doPostHeaderRequest($uri, array $header = array(), array $data = array()) {
   $curl = curl_init($uri);
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($curl, CURLOPT_FORBID_REUSE, true);
-  curl_setopt($curl, CURLOPT_FRESH_CONNECT, true);
-  curl_setopt($curl, CURLOPT_HEADER, true);
+  curl_setopt($curl, CURLOPT_POST, TRUE);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_FORBID_REUSE, TRUE);
+  curl_setopt($curl, CURLOPT_FRESH_CONNECT, TRUE);
+  curl_setopt($curl, CURLOPT_HEADER, TRUE);
   curl_setopt($curl, CURLOPT_TIMEOUT, 4);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
   curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-  $return = curl_exec($curl);
+  $ret = curl_exec($curl);
   curl_close($curl);
-  print_r($return);
-  return json_decode($return, true);
+  $return = json_decode($ret, TRUE);
+  if (isset($return['error'])) {
+    return FALSE;
+  } else {
+    return $return;
+  }
 }
 
 function scandirStrict($folder) {
