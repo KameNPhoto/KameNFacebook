7576c9d85e94f7465ad90a5558d776e643540fdd
Damien Lesgourgues <damien.lesgourgues@believedigital.com>
2017-12-06 17:54:06 +0100

    Move file if upload is verified

--- file.php
+++ file.php
@@ -1,12 +1,14 @@
 <?php
 
-function moveFileToPublished($file, $dest) {
-  global $debug;
-  global $config;
-  return
+function moveFileToPublished($src, $dest) {
+  preg_match('#(/.+)/(.+?)$#',$src,$s);
+  preg_match('#(/.+)/(.+?)$#',$dest,$d);
+  if (! is_dir($d[1])) {
+    echo "Creating destination directory : ".$d[1].PHP_EOL;
+    mkdir($d[1], 0755, TRUE);
+  }
+  echo "Prepare to move ".$s[2]." from folder ".$s[1]." to folder ".$d[1].PHP_EOL;
+  copy($src, $dest);
+  unlink($src);
+  return;
 }
-function checkPathexist($path) {
-  global $debug;
-  global $config;
-}
-
