eb9a9d088e6a2f045b2053af7d5b5acfa458d494
Damien Lesgourgues <damien.lesgourgues@believedigital.com>
2017-12-06 13:25:09 +0100

    Add explication of script

--- fbapp.php
+++ fbapp.php
@@ -1,8 +1,7 @@
 <?php
-
+// This page need to be hosted on a public server and configured as callback on facebook app properties.
 echo "<html><head><title>Kame'N Photo Facebook App</title></head>";
 echo "<body><p>Enter the following code in the application :<br /></p><p>";
 echo '<textarea rows="6" cols="80">'.$_GET['code'].'</textarea>';
 echo "</p></body></html>";
-
 ?>
