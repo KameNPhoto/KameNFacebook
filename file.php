5001a951891a00a164cdef7986f6ad1d7246ef6c
Damien Lesgourgues <damien.lesgourgues@believedigital.com>
2017-12-11 17:09:23 +0100

    Clear file information cache in PHP when moving or deleting files

--- file.php
+++ file.php
@@ -10,5 +10,6 @@ function moveFileToPublished($src, $dest) {
   echo "Prepare to move ".$s[2]." from folder ".$s[1]." to folder ".$d[1].PHP_EOL;
   copy($src, $dest);
   unlink($src);
+  clearstatcache();
   return;
 }
