0a53e76a0cf338dfae743e3181e3580feda0717e
Damien Lesgourgues <damien@lesgourgues.fr>
2018-05-16 11:39:35 +0200

    basic watermark management

--- file.php
+++ file.php
@@ -18,6 +18,8 @@ function resizeImageFromFolder($folder, $resize) {
   global $debug;
   $sourceFolder = $folder;
   $resizeFolder = $folder.'/'.$resize;
+  $watermarkFile = $folder.'/../water.png';
+  $waterOffset = 0;
   $dir = scandirStrict($sourceFolder);
   if (!is_dir($resizeFolder)) { mkdir($resizeFolder, 0755, TRUE); }
   foreach($dir as $photo) {
@@ -26,6 +28,18 @@ function resizeImageFromFolder($folder, $resize) {
     if ($debug) { fwrite(STDERR, "Resizing image ".$photo.PHP_EOL); }
     $resized = new Imagick($sourceFolder."/".$photo);
     $resized->resizeImage($resize,$resize,Imagick::FILTER_CATROM,1,TRUE);
+    if (file_exists($watermarkFile)) {
+      if ($debug) { fwrite(STDERR, "Adding watermark to ".$photo.PHP_EOL); }
+      $watermark = new Imagick();
+      $watermark->readImage($watermarkFile);
+      $img_Width = $resized->getImageWidth();
+      $img_Height = $resized->getImageHeight();
+      $watermark_Width = $watermark->getImageWidth();
+      $watermark_Height = $watermark->getImageHeight();
+      $x = ($img_Width - $watermark_Width - $waterOffset);
+      $y = $waterOffset;
+      $resized->compositeImage($watermark, imagick::COMPOSITE_OVER, $x, $y);
+    }
     $resized->writeImage($resizeFolder."/".$photo);
     echo ".";
   }
